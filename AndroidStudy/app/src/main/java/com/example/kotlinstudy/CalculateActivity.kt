5072559639c6588da8775caa537e6981ad052199
kimsubeen <papajj06@naver.com>
2022-01-29 03:10:02 +0900

    [modify] null  bug 수정

--- AndroidStudy/app/src/main/java/com/example/kotlinstudy/CalculateActivity.kt
+++ AndroidStudy/app/src/main/java/com/example/kotlinstudy/CalculateActivity.kt
@@ -1,7 +1,11 @@
 package com.example.kotlinstudy
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.kotlinstudy.databinding.ActivityCalculateBinding
 import com.example.kotlinstudy.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -14,24 +18,43 @@ class CalculateActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculateBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.etText.setOnClickListener{ calculateTip() }
+        binding.btnCalculate.setOnClickListener { calculateTip() }
+        binding.etText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
     }
 
-    fun calculateTip(){
-        val cost = binding.etText.text.toString().toDouble()
-        val checkId = binding.radio.checkedRadioButtonId
-        val tipPercentage = when(checkId){
+    private fun calculateTip() {
+        val cost = binding.etText.text.toString().toDoubleOrNull()
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
+            return
+        }
+        val tipPercentage = when (binding.radio.checkedRadioButtonId) {
             R.id.btn_amazing -> 0.20
             R.id.btn_good -> 0.18
             else -> 0.15
         }
         var tip = tipPercentage * cost
-        val roundUp = binding.btnSwitch.isChecked
-        if(roundUp){
+        if (binding.btnSwitch.isChecked) {
             tip = ceil(tip)  // ceil은 상한, 위로만 반올림.
         }
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tvResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
