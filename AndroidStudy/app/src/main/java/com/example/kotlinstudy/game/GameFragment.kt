0d5d9f5a372d3cdcb44b48041a023cdef83972c2
kimsubeen <papajj06@naver.com>
2022-02-07 16:47:55 +0900

    [modify] LiveData로 변경

--- AndroidStudy/app/src/main/java/com/example/kotlinstudy/game/GameFragment.kt
+++ AndroidStudy/app/src/main/java/com/example/kotlinstudy/game/GameFragment.kt
@@ -1,12 +1,11 @@
 package com.example.kotlinstudy.game
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContentProviderCompat
+import androidx.databinding.DataBindingUtil
 import com.example.kotlinstudy.R
 import com.example.kotlinstudy.databinding.FragmentGameBinding
 import androidx.fragment.app.viewModels
@@ -22,30 +21,50 @@ class GameFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentGameBinding.inflate(inflater, container, false)
+//        binding = FragmentGameBinding.inflate(inflater, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.gameViewModel = viewModel
+        binding.maxNoOfWords = MAX_NO_OF_WORDS
+        binding.lifecycleOwner = viewLifecycleOwner
+
         binding.submit.setOnClickListener { onSubmitWord() }
         binding.skip.setOnClickListener { onSkipWord() }
-        updateNextWordOnScreen()
-        binding.score.text = getString(R.string.score, 0)
-        binding.wordCount.text = getString(
-            R.string.word_count, 0, MAX_NO_OF_WORDS
-        )
+//        updateNextWordOnScreen()
+//        binding.score.text = getString(R.string.score, 0)
+//        binding.wordCount.text = getString(
+//            R.string.word_count, 0, MAX_NO_OF_WORDS
+//        )
+        //updateNextWordOnScreen메소드 대신에 livedata로 변경
+        // -> view에 livedata 객체 바인딩해줘서 삭제함.
+//        viewModel.currentScrambledWord.observe(viewLifecycleOwner,
+//            { newWord ->
+//                binding.textViewUnscrambledWord.text = newWord
+//            })
+        //score
+//        viewModel.score.observe(viewLifecycleOwner,
+//            { newScore ->
+//                binding.score.text = getString(R.string.score, newScore)
+//            })
+        //wordCount
+//        viewModel.currentWordCount.observe(viewLifecycleOwner,
+//            { newWordCount ->
+//                binding.wordCount.text =
+//                    getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS)
+//            })
     }
 
     private fun onSubmitWord() {
         val playerWord = binding.textInputEditText.text.toString()
 
-        if (viewModel.isUserWordCorrect(playerWord)) { //뷰모델에 있는 함수 사용 , 제출 시 단어가 맞는지 확인여부 함수
-            setErrorTextField(false) //만약 true이면 오류 메시지 안나타남
-            if (viewModel.nextWord()) { //다음 단어 보여주기기
-                updateNextWordOnScreen()
-            } else {
+        if (viewModel.isUserWordCorrect(playerWord)) {
+            setErrorTextField(false)
+            if (!viewModel.nextWord()) {
                 showFinalScoreDialog()
             }
         } else {
@@ -53,64 +72,56 @@ class GameFragment : Fragment() {
         }
     }
 
-    private fun onSkipWord() {
-        if (viewModel.nextWord()) {
+        private fun onSkipWord() {
+            if (viewModel.nextWord()) {
+                setErrorTextField(false)
+                //updateNextWordOnScreen()
+            } else {
+                showFinalScoreDialog()
+            }
+        }
+
+
+        private fun restartGame() {
+           viewModel.reinitializeData()
             setErrorTextField(false)
-            updateNextWordOnScreen()
-        } else {
-            showFinalScoreDialog()
+            //updateNextWordOnScreen()
+        }
+
+        private fun exitGame() {
+            activity?.finish()
+        }
+
+
+        private fun setErrorTextField(error: Boolean) {
+            if (error) {
+                binding.textField.isErrorEnabled = true
+                binding.textField.error = getString(R.string.try_again)
+            } else {
+                binding.textField.isErrorEnabled = false
+                binding.textInputEditText.text = null
+            }
+        }
+
+//    private fun updateNextWordOnScreen() {
+//        binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
+//    }
+
+        private fun showFinalScoreDialog() {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(getString(R.string.congratulations))
+                .setMessage(getString(R.string.you_scored, viewModel.score.value))
+                .setCancelable(false)
+                .setNegativeButton(getString(R.string.exit)) { _, _ ->
+                    exitGame()
+                }
+                .setPositiveButton(getString(R.string.play_again)) { _, _ ->
+                    restartGame()
+                }
+                .show()
         }
     }
 
-    private fun getNextScrambledWord(): String {
-        val tempWord = allWordsList.random().toCharArray()
-        tempWord.shuffle()
-        return String(tempWord)
-    }
-
-    private fun restartGame() {
-        viewModel.reinitializeData()
-        setErrorTextField(false)
-        updateNextWordOnScreen()
-    }
-
-    private fun exitGame() {
-        activity?.finish()
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        Log.d("GameFragment", "GameFragment destroyed!")
-    }
-
-    private fun setErrorTextField(error: Boolean) {
-        if (error) {
-            binding.textField.isErrorEnabled = true
-            binding.textField.error = getString(R.string.try_again)
-        } else {
-            binding.textField.isErrorEnabled = false
-            binding.textInputEditText.text = null
-        }
-    }
-
-    private fun updateNextWordOnScreen() {
-        binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
-    }
-
-    private fun showFinalScoreDialog() {
-        MaterialAlertDialogBuilder(requireContext())
-            .setTitle(getString(R.string.congratulations))
-            .setMessage(getString(R.string.you_scored, viewModel.score))
-            .setCancelable(false)
-            .setNegativeButton(getString(R.string.exit)) { _, _ ->
-                exitGame()
-            }
-            .setPositiveButton(getString(R.string.play_again)) { _, _ ->
-                restartGame()
-            }
-            .show()
-    }
-}
 
 // 뷰모델 적용 전 함수들
 /*
--- AndroidStudy/app/src/main/java/com/example/kotlinstudy/game/GameViewModel.kt
+++ AndroidStudy/app/src/main/java/com/example/kotlinstudy/game/GameViewModel.kt
@@ -1,20 +1,22 @@
 package com.example.kotlinstudy.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
-    private var _score = 0
-    val score: Int
+    private val _score = MutableLiveData(0)
+    val score: LiveData<Int>
         get() = _score
 
-    private var _currentWordCount = 0
-    val currentWordCount: Int
+    private val _currentWordCount = MutableLiveData(0)
+    val currentWordCount: LiveData<Int>
         get() = _currentWordCount
 
-    private lateinit var _currentScrambledWord: String
-    val currentScrambledWord: String
+    private val _currentScrambledWord = MutableLiveData<String>()
+    val currentScrambledWord: LiveData<String>
         get() = _currentScrambledWord
 
     private var wordsList: MutableList<String> = mutableListOf()
@@ -23,11 +25,6 @@ class GameViewModel : ViewModel() {
     init {
         Log.d("GameFragment", "GameViewModel created!")
         getNextWord()
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        Log.d("GameFragment", "GameViewModel destroyed!")
     }
 
     private fun getNextWord() {
@@ -41,28 +38,28 @@ class GameViewModel : ViewModel() {
         if (wordsList.contains(currentWord)) { //만약 이미 냈던 단어였으면 다음 단어 호출
             getNextWord()
         } else {
-            _currentScrambledWord = String(tempWord)
-            ++_currentWordCount
+            _currentScrambledWord.value = String(tempWord)
+            _currentWordCount.value = (_currentWordCount.value)?.inc()
             wordsList.add(currentWord)
         }
     }
 
     fun reinitializeData() { //다시 시작 초기화.
-        _score = 0
-        _currentWordCount = 0
+        _score.value = 0
+        _currentWordCount.value = 0
         wordsList.clear()
         getNextWord()
     }
 
     fun nextWord(): Boolean {
-        return if (_currentWordCount < MAX_NO_OF_WORDS) {
+        return if (_currentWordCount.value!! < MAX_NO_OF_WORDS) {
             getNextWord()
             true
         } else false
     }
 
     private fun increaseScore() {
-        _score += SCORE_INCREASE
+        _score.value = (_score.value)?.plus(SCORE_INCREASE)
     }
 
     fun isUserWordCorrect(playerWord: String): Boolean {
@@ -72,5 +69,7 @@ class GameViewModel : ViewModel() {
         }
         return false
     }
-
 }
+
+
+
